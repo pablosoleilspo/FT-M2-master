85650f2f5c00210abdd20fe98942536c966084d4
pablo vasquez <pablosoleilspo@gmail.com>
2022-09-15 15:26:14 -0500

    M2

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,17 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if (matchFunc(startEl)) resultSet.push(startEl)
+
+
+  for (let i = 0; i < startEl.children.length; i++) {
+
+    let childrenResult = traverseDomAndCollectElements(matchFunc, startEl.children[i])
+
+    resultSet = [...resultSet, ...childrenResult] // [,]
+  }
+
+  return resultSet
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +28,11 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
+  if(selector[0] === "#") return "id";
+  if(selector[0] === ".") return "class";
+  if(selector.includes('.')) return 'tag.class';
   
+  return 'tag';
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,14 +44,48 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+     // funcion que es la herramienta para ir a buscar el selector al dom
+     matchFunction = (el) => {
+      // #pepito
+      // el -> <div id="pepito">asdadaad</div>
+      // el.id = "pepito"
+      // #pepito === pepito
+      return "#" + el.id === selector
+     }
+
   } else if (selectorType === "class") {
+      matchFunction = (el) => {
+        //el = {id:"", classList:[juanita, section, .parrafo]}
+  
+        for (let i = 0; i < el.classList.length; i++) {
+  
+          if ("." + el.classList[i] === selector) return true
+        }
+        return false
+      }
     
   } else if (selectorType === "tag.class") {
-    
+        //[tag, class]
+        matchFunction = (el) => {
+          
+                let [tag, clase] = selector.split(".")
+                // let tag = arr[0]
+                // let clase = arr[1]
+          
+                // pporque debe buscar tag y class y ya tenemos definido como buscar cada una de ellas
+                // Porque en la funcioon functionmartchmaker estamos buscando un selector con el elemento, esta vez estamos buscando que coincida la primer parte con el elemento y luego lo mismo, pero con la segunda parte
+          
+                return matchFunctionMaker(tag)(el) && matchFunctionMaker("." + clase)(el)
+              }
+              
+          
   } else if (selectorType === "tag") {
-    
+    matchFunction = (el) => {
+      return el.tagName.toLowerCase() === selector
+    }
+
   }
+  
   return matchFunction;
 };
 
